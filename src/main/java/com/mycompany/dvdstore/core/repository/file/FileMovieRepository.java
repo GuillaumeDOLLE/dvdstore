20c512e61a800f2a3a54a9932c37408d37cba94b
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-05-19 15:58:13 +0200

    Initial commit after multiples TPs

---
+++ src/main/java/com/mycompany/dvdstore/core/repository/file/FileMovieRepository.java
@@ -0,0 +1,60 @@
+package com.mycompany.dvdstore.core.repository.file;
+
+import com.mycompany.dvdstore.core.entity.Movie;
+import com.mycompany.dvdstore.core.repository.MovieRepositoryInterface;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class FileMovieRepository implements MovieRepositoryInterface {
+
+    private static List<Movie> movies = new ArrayList<>();
+    @Value("${movies.file.location}")
+    private File file;
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public void add(Movie movie) {
+        FileWriter writer;
+        try {
+            writer = new FileWriter(file, true);
+            writer.write(movie.getTitle() + ";" + movie.getGenre());
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("The movie " + movie.getTitle() + " has been added.");
+    }
+
+    @Override
+    public List<Movie> list() {
+
+        List<Movie> movies=new ArrayList<>();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+            for(String line; (line = br.readLine()) != null; ) {
+                final Movie movie=new Movie();
+                final String[] titreEtGenre = line.split("\\;");
+                movie.setTitle(titreEtGenre[0]);
+                movie.setGenre(titreEtGenre[1]);
+                movies.add(movie);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return movies;
+    }
+
+}
---
+++ src/main/java/com/mycompany/dvdstore/core/repository/memory/MemoryMovieRepository.java
@@ -0,0 +1,23 @@
+package com.mycompany.dvdstore.core.repository.memory;
+
+import com.mycompany.dvdstore.core.entity.Movie;
+import com.mycompany.dvdstore.core.repository.MovieRepositoryInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//@Repository
+public class MemoryMovieRepository implements MovieRepositoryInterface {
+
+    private static List<Movie> movies = new ArrayList<>();
+
+    public void add(Movie movie) {
+        movies.add(movie);
+        System.out.println("The movie " + movie.getTitle() + " has been added.");
+    }
+
+    @Override
+    public List<Movie> list() {
+        return movies;
+    }
+}
