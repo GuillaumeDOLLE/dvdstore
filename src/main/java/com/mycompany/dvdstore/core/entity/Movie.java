f6b0d3312c2af414cfeae43ff9f8cf57357a337b
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-05-25 15:06:48 +0200

    TP31, Spring data JPA

--- src/main/java/com/mycompany/dvdstore/core/entity/Movie.java
+++ src/main/java/com/mycompany/dvdstore/core/entity/Movie.java
@@ -1,10 +1,12 @@
 package com.mycompany.dvdstore.core.entity;
 
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.*;
 
+@Entity
 public class Movie {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String genre;
